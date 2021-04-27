1814b60e3329433b1f847fbd5d6ed264f9129c54
Thiskk040 <78652197+Thiskk040@users.noreply.github.com>
2021-04-28 01:13:27 +0700

    Add files via upload
    คุณานนต์ 63120501040

---
+++ ข้อ 1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row;
+            Console.Write("Please insert the number to make the pascla triangle : ");
+            row = int.Parse(Console.ReadLine());
+            if(row>=0)
+            {
+                thetrianglepascal(row);
+            }
+            else if(row<=0)
+            {
+                Console.WriteLine("Invalid Pascal Traingle");
+                Console.Write("Please insert the number to make the pascla triangle again!!! : ");
+                row = int.Parse(Console.ReadLine());
+                thetrianglepascal(row);
+            }
+
+        }
+        static void thetrianglepascal(int row)
+        {
+            int value;
+            value = 1;
+
+            for (int i = 0; i <= row; i++)
+            {
+
+                for (int j = 0; j <= i; j++)
+                {
+
+                    if (j == 0 || i == 0)
+                        value = 1;
+                    else
+                        value = value * (i - j + 1) / j;
+                    Console.Write(value + " ");
+                }
+                Console.WriteLine();
+
+            }
+        }
+
+    }
+
+    
+    
+}
+
+
+
